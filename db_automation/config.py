f098fa7ef9b5103026bb031e27ff207760a71774
Lenny <data@cforent.be>
2026-03-31 12:24:36 +0100

    [Update] - Added constant 'CBE_DB'.

--- db_automation/config.py
+++ db_automation/config.py
@@ -9,40 +9,41 @@ class Config:
     """A class to hold all configuration variables."""
 
     # Debug
-    DEBUG = bool(str(os.getenv("DEBUG")) == '1')
+    DEBUG = bool(os.getenv("DEBUG")) == '1'
 
     # Database module
-    DB_USERNAME = str(os.getenv("DB_USERNAME", ""))
-    DB_PASSWORD = str(os.getenv("DB_PASSWORD", ""))
-    DB_HOST = str(os.getenv("DB_HOST", ""))
+    DB_USERNAME = os.getenv("DB_USERNAME", "")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_HOST = os.getenv("DB_HOST", "")
     DB_PORT = int(os.getenv("DB_PORT", ""))
-    DB_NBB = str(os.getenv("DB_NBB"))
-    DB_ARCHIVE = str(os.getenv("DB_ARCHIVE"))
+    DB_NBB = os.getenv("DB_NBB")
+    DB_ARCHIVE = os.getenv("DB_ARCHIVE")
 
     # Logger module
-    LOG_CONFIG = str(os.getenv("LOG_CONFIG", ""))
-    LOG_UPDATE_CBE = str(os.getenv("LOG_UPDATE_CBE", ""))
-    LOG_UPDATE_NBB = str(os.getenv("LOG_UPDATE_NBB", ""))
-    LOG_MAIL = str(os.getenv("LOG_MAIL", ""))
+    LOG_CONFIG = os.getenv("LOG_CONFIG", "")
+    LOG_UPDATE_CBE = os.getenv("LOG_UPDATE_CBE", "")
+    LOG_UPDATE_NBB = os.getenv("LOG_UPDATE_NBB", "")
+    LOG_MAIL = os.getenv("LOG_MAIL", "")
 
     # Mailer module
-    MAIL_HOST = str(os.getenv("MAIL_HOST", ""))
+    MAIL_HOST = os.getenv("MAIL_HOST", "")
     MAIL_PORT = int(os.getenv("MAIL_PORT", 1))
-    MAIL_USER = str(os.getenv("MAIL_USER", ""))
-    MAIL_PASSWORD = str(os.getenv("MAIL_PASSWORD", ""))
-    MAIL_RECEIVER = str(os.getenv("MAIL_RECEIVER", ""))
+    MAIL_USER = os.getenv("MAIL_USER", "")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
+    MAIL_RECEIVER = os.getenv("MAIL_RECEIVER", "")
 
     # CBE updater module
-    USER_AGENT = str(os.getenv("USER_AGENT", ""))
-    CBE_USER = str(os.getenv("CBE_USER", ""))
-    CBE_PASSWORD = str(os.getenv("CBE_PASSWORD", ""))
-    URL_LOGIN = str(os.getenv("URL_LOGIN", ""))
-    URL_ZIP = str(os.getenv("URL_ZIP", ""))
-    ZERO_DATE = str(os.getenv("ZERO_DATE", ""))
-    ZERO_POINT = str(os.getenv("ZERO_POINT", ""))
-    ZIP_DESTINATION = str(os.getenv("ZIP_DESTINATION", ""))
-    ZIP_FILENAME = str(os.getenv("ZIP_FILENAME", ""))
+    USER_AGENT = os.getenv("USER_AGENT", "")
+    CBE_USER = os.getenv("CBE_USER", "")
+    CBE_PASSWORD = os.getenv("CBE_PASSWORD", "")
+    URL_LOGIN = os.getenv("URL_LOGIN", "")
+    URL_ZIP = os.getenv("URL_ZIP", "")
+    ZERO_DATE = os.getenv("ZERO_DATE", "")
+    ZERO_POINT = os.getenv("ZERO_POINT", "")
+    ZIP_DESTINATION = os.getenv("ZIP_DESTINATION", "")
+    ZIP_FILENAME = os.getenv("ZIP_FILENAME", "")
+    CBE_DB = os.getenv("CBE_DB", "")
 
     # NBB updater module
-    API_KEY_AUTH = str(os.getenv("API_KEY_AUTH", ""))
-    API_KEY_EXTR = str(os.getenv("API_KEY_EXTR", ""))
+    API_KEY_AUTH = os.getenv("API_KEY_AUTH", "")
+    API_KEY_EXTR = os.getenv("API_KEY_EXTR", "")
