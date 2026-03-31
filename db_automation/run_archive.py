cb44fd7fec2c567754787c3241215e3451647c11
Lenny <data@cforent.be>
2026-03-31 17:30:27 +0100

    [Update] - Refactored to work with the newly, more generic function 'send_update_mail'.

--- db_automation/run_archive.py
+++ db_automation/run_archive.py
@@ -1,8 +1,10 @@
 import sys
 
+from db_automation.config import Config
 from db_automation.logger.config import update_nbb_logger
 from db_automation.updater.archive.fetch_extracts import fetch_extracts
 from db_automation.updater.archive.update import update
+from db_automation.mailer.send_mail import send_update_mail
 
 
 def main():
@@ -10,7 +12,8 @@ def main():
 
     try:
         fetch_extracts()
-        update()
+        status = update()
+        send_update_mail(status, Config.LOG_UPDATE_NBB)
     except Exception as e:
         update_nbb_logger.error(
             'The following error caused the script to stop'
