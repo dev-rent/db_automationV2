d45ecd0999a50b6652c276f12860bc3751cba0d0
Lenny <data@cforent.be>
2026-04-01 13:23:30 +0100

    [Milestone] - Refactored version of archive updater. Main changes involve restructuring files and reduce levels of logic. 'run_archive.py' calls upon handlers with clear description of what they do. Some handlers use functions (functions.py) that can be reused.

--- db_automation/updater/archive/functions.py
+++ db_automation/updater/archive/functions.py
@@ -3,61 +3,32 @@ import json
 import time
 from datetime import datetime
 
+from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import insert
+
+import db_automation.database.models_archive as mdl
 from db_automation.api.classes import QueryNbbConsult
+from db_automation.database.classes import DbConnector
 from db_automation.logger.config import update_nbb_logger
 
 
-def extracts_call(
-    dest_zip: str,
-    date: str,
-    references=True
-) -> bool:
-    """Fetch new update files from NBB API. Return boolean for success.
+def get_URL(obj) -> list:
+    lst = []
 
-    Parameters
-    ----------
-    references : bool
-        Default `True` and fetches references. Else filings.
-    dest_zip : str
-        Destination for ZIP-file.
-    """
+    if isinstance(obj, dict):
+        x = obj.get('ReferenceNumber')
+        if x:
+            lst = [x]
+        return lst
 
-    i = 1
-    attempt = True
-    success = False
-
-    if references:
-        request = "ref"
-    else:
-        request = "accData"
-
-    # Fetch reference data from API
-    while attempt:
-        try:
-            api = QueryNbbConsult(
-                {
-                    "db": "extracts",
-                    "request": request,
-                    "date": date
-                }
-            )
-            time.sleep(2)  # Prevents error requests.exceptions.ConnectionError: HTTPSConnectionPool(host='ws.cbso.nbb.be', port=443): Max retries exceeded with url: /extracts/batch/2026-03-14/references (Caused by NameResolutionError("HTTPSConnection(host='ws.cbso.nbb.be', port=443): Failed to resolve 'ws.cbso.nbb.be' ([Errno 8] nodename nor servname provided, or not known)"))
-            with open(dest_zip, "wb") as f:
-                f.write(api.response.content)
-
-            attempt = False
-            success = True
-        except Exception as e:
-            update_nbb_logger.error(
-                "Failed attempt %s, fetching %s", i, request
-            )
-            update_nbb_logger.exception(e)
-            time.sleep(1)
-            i += 1
-            if i > 3:
-                attempt = False
-                success = False
-    return success
+    elif isinstance(obj, list):
+        lst = [
+            d['ReferenceNumber']
+            for d in obj
+            if d.get('ReferenceNumber')
+        ]
+        return lst
+    return lst
 
 
 def upper_first_char_keys(lst: list):
@@ -75,19 +46,133 @@ def upper_first_char_keys(lst: list):
         return lst
 
 
-def standarise_keys(path: str):
-    """Use UpperCamelCase for all dictionary keys."""
+def map_folder(folder: str, *, extension: str) -> set[tuple[str, str]]:
+    """Return iterator of given folder."""
 
-    with os.scandir(path) as it:
+    tuple_set = set()
+
+    with os.scandir(folder) as it:
         for entry in it:
-            if entry.is_file() and entry.name.endswith('.json'):
-                with open(entry.path, 'r') as f:
-                    data = json.load(f)
+            if entry.is_file() and entry.name.endswith(extension):
+                with open(entry.path, 'r') as data:
+                    file = json.load(data)
+                    e_id = file.get("EnterpriseNumber")
+                    f_id = file.get("ReferenceNumber")
 
-                new_data = upper_first_char_keys(data)
+                    if e_id and f_id:
+                        tuple_set.add((e_id, f_id))
+                    else:
+                        update_nbb_logger.error(
+                            "Keys not found. %s: %s", e_id, f_id
+                        )
+                        continue
 
-                with open(entry.path, 'w') as f:
-                    f.write(json.dumps(new_data))
+    return tuple_set
+
+
+def fetch_current_value(e_id: str, db_engine: DbConnector) -> list | None:
+    """Return current value in database."""
+
+    stmt = (
+        select(mdl.Reference.json_reference)
+        .where(mdl.Reference.enterprise_id == e_id)
+    )
+
+    with db_engine.engine.begin() as conn:
+        current_value = conn.scalar(stmt)
+    return current_value
+
+
+def first_entry(e_id: str, db_engine: DbConnector):
+    """Request and insert data as if a first entry."""
+
+    i = 0
+
+    while i < 3:
+        try:
+            call = QueryNbbConsult(
+                {
+                    "db": "authentic",
+                    "request": "ref",
+                    "ref_id": e_id
+                }
+            )
+            response = call.response.json()
+            break
+        except Exception as e:
+            time.sleep(3)
+
+            i += 1
+            if i >= 3:
+                raise Exception(f"Failed inserting first entry {e_id}: {e}")
+
+    references = upper_first_char_keys(response)
+
+    if isinstance(references, dict):
+        references = [references]
+    references = sort_keep_lastest(references)
+
+    # First phase: insert known references.
+    stmt = (
+        insert(mdl.Reference)
+        .values({
+            "enterprise_id": e_id,
+            "json_reference": references,
+            "last_update": datetime.now()
+        })
+    )
+    stmt = stmt.on_conflict_do_update(
+        index_elements=["enterprise_id"],
+        set_={
+            "json_reference": stmt.excluded.json_reference,
+            "last_update": stmt.excluded.last_update
+        }
+    )
+
+    with db_engine.engine.begin() as conn:
+        conn.execute(stmt)
+
+    # Second phase: inserting known statements.
+    filing_ids = get_URL(references)
+    filings_to_commit = []
+
+    for f_id in filing_ids:
+        call = QueryNbbConsult(
+            {
+                "db": "authentic",
+                "request": "accData",
+                "ref_id": f_id
+            }
+        )
+        response = call.response
+
+        if response.status_code != 200:
+            continue
+
+        r = response.json()
+
+        stmt = (
+            insert(mdl.Filing)
+            .values({
+                "enterprise_id": e_id,
+                "filing_id": f_id,
+                "json_filing": r,
+                "last_update": datetime.now()
+            })
+        )
+        stmt = stmt.on_conflict_do_update(
+            index_elements=['enterprise_id', 'filing_id'],
+            set_={
+                "json_filing": stmt.excluded.json_filing,
+                "last_update": stmt.excluded.last_update
+            }
+        )
+        filings_to_commit.append(stmt)
+
+    with db_engine.engine.begin() as conn:
+        for stmt in filings_to_commit:
+            conn.execute(stmt)
+        # implied conn.commit()
 
 
 def sort_keep_lastest(ref_list: list) -> list:
@@ -105,3 +190,18 @@ def sort_keep_lastest(ref_list: list) -> list:
         reverse=True
         )
     return lst
+
+
+def delete_(path: str):
+    """Delete file/folder at given path.
+
+    Params
+    ------
+    path : str
+        path to delete.
+    """
+
+    try:
+        os.remove(path)
+    except Exception as e:
+        raise Exception(f"Failed deleting path: {e}")
