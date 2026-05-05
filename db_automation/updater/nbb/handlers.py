7316e67af09c3853bf139663a5ac93b0e0faf0e3
Lenny <data@cforent.be>
2026-05-05 18:49:52 +0100

    [Update] - Refactoring populate_destination_references(). Handles Reference dataclass.

--- db_automation/updater/nbb/handlers.py
+++ db_automation/updater/nbb/handlers.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
-from sqlalchemy import select
-from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy import String, any_, bindparam, select
+from sqlalchemy.dialects.postgresql import ARRAY, insert
 
 import db_automation.database.models_archive as mdl_a
 import db_automation.database.models_nbb as nbb
+from db_automation.updater.nbb.classes import Reference
 import db_automation.updater.nbb.functions as fnc
 from db_automation.database.classes import DbConnector
 from db_automation.logger.config import update_nbb_logger
@@ -68,101 +69,114 @@ def prepare_update(db_from: str, db_to: str) -> set[str]:
     return stale_ids
 
 
-# def references_cd_nbb_data(
-#     db_from: str,
-#     db_to: str,
-#     # to_update: set,
-#     country_codes: dict
-# ):
+def populate_destination_references(
+    db_from: str,
+    db_to: str,
+    to_update: set[str]
+):
+    """Populate all references in CompanyData database."""
+
+    source_db = DbConnector(db=db_from)
+    dest_db = DbConnector(db=db_to)
+
+    batch_size = 500
+    ci_batch = []
+    statement_batch = []
+
+    stmt = (
+        select(mdl_a.Reference.enterprise_id, mdl_a.Reference.json_reference)
+        .where(
+            mdl_a.Reference.enterprise_id == any_(
+                bindparam("ids", value=list(to_update), type_=ARRAY(String))
+            )
+        )
+    )
+
+    with source_db.engine.connect() as conn_from:
+        result = (
+            conn_from.execution_options(stream_results=True)
+            .execute(stmt)
+            .mappings()
+        )
+
+        for row in result:
+            enterprise_id: str = row["enterprise_id"]
+            references: list[Reference] | None = (
+                [Reference.from_dict(item) for item in row['json_reference']] 
+                if row.get("json_reference") 
+                else None
+            )
+
+            if not references:
+                update_nbb_logger.info(
+                    f"Skipped {enterprise_id} because no reference or reference is empty"
+                )
+                continue
+
+            latest_references = fnc.sort_keep_lastest(references)
+
+            if not latest_references:
+                update_nbb_logger.info("Skipped %s — all references before 2021", enterprise_id)
+                continue
+
+            latest = latest_references[0]
+            update_nbb_logger.info("")
+            update_nbb_logger.info(
+                "------------Trying new enterprise: %s ------------", enterprise_id
+            )
+
+            ci_batch.append({
+                "enterprise_id": latest.enterprise_number,
+                "denomination": latest.enterprise_name,
+                "legal_situation": latest.legal_situation,
+                "search_field": (
+                    ''.join(latest.enterprise_name.split())
+                    if latest.enterprise_name else None
+                ),
+                "last_update": datetime.now()
+            })
+
+            for ref in references:
+                statement_batch.append({
+                    "filing_id": ref.reference_number,
+                    "enterprise_id": ref.enterprise_number,
+                    "start_date": ref.exercise_dates.start_date,
+                    "end_date": ref.exercise_dates.end_date,
+                    "account_year": ref.account_year,
+                    "deposit_date": ref.deposit_date,
+                    "deposit_type": ref.deposit_type,
+                    "currency": ref.currency,
+                    "legal_form": ref.legal_form,
+                    "activity_code": ref.activity_code,
+                    "model_type": ref.model_type,
+                    "account_url": bool(ref.account_data_url),
+                    "legal_validation": ref.legal_validation,
+                    "assembly_date": ref.assembly_date,
+                    "data_version": ref.data_version,
+                    "improvement_date": ref.improvement_date,
+                    "corrected_data": ref.corrected_data,
+                    "last_update": datetime.now(),
+                })
+
+            if len(statement_batch) >= batch_size:
+                fnc.write_batch(
+                    engine=dest_db.engine,
+                    ci_batch=ci_batch,
+                    statement_batch=statement_batch
+                )
+                ci_batch.clear()
+                statement_batch.clear()
+
+        if ci_batch:
+            fnc.write_batch(
+                engine=dest_db.engine,
+                ci_batch=ci_batch,
+                statement_batch=statement_batch
+            )
+
+
+# def populate_destination_references(db_from: str, db_to: str):
 #     """Populate all references in CompanyData database."""
-
-#     database_from = DbConnector(db=db_from)
-#     database_to = DbConnector(db=db_to)
-
-#     batch_size = 100
-#     ci_batch = []
-#     statement_batch = []
-
-#     stmt = (
-#         select(mdl_a.Reference.enterprise_id, mdl_a.Reference.json_reference)
-#         .where(
-#             mdl_a.Reference.json_reference.is_not(None)
-#             # mdl_a.Reference.enterprise_id.in_(to_update)
-#         )
-#     )
-
-#     with database_from.engine.connect() as conn_from:
-#         result = (
-#             conn_from.execution_options(stream_results=True)
-#             .execute(stmt)
-#             .mappings()
-#         )
-
-#         for row in result:
-#             enterprise_id = row["enterprise_id"]
-#             json_reference = row["json_reference"]
-#             json_reference = fnc.sort_keep_lastest(json_reference)
-
-#             if not json_reference:
-#                 # TODO: write function to delete empty list for null-value
-#                 update_nbb_logger.info(f"Skipped {enterprise_id} because empty")
-#                 continue
-
-#             update_nbb_logger.info("")
-#             update_nbb_logger.info(
-#                 "------------Trying new enterprise: %s ------------", enterprise_id
-#             )
-
-#             latest = max(json_reference, key=lambda x: x["DepositDate"])
-
-#             ci_batch.append({
-#                 "enterprise_id": enterprise_id,
-#                 "denomination": latest.get("EnterpriseName"),
-#                 "legal_situation": latest.get("LegalSituation"),
-#                 "last_update": datetime.now()
-#             })
-
-#             for ref in json_reference:
-#                 statement_batch.append({
-#                     "filing_id": ref.get("ReferenceNumber"),
-#                     "enterprise_id": ref.get("EnterpriseNumber"),
-#                     "start_date": fnc.to_datetime(ref["ExerciseDates"].get("StartDate")),
-#                     "end_date": fnc.to_datetime(ref["ExerciseDates"].get("EndDate")),
-#                     "account_year": fnc.to_datetime(ref.get("DepositDate"), year_only=True),
-#                     "deposit_date": fnc.to_datetime(ref.get("DepositDate")),
-#                     "deposit_type": ref.get("DepositType"),
-#                     "currency": ref.get("Currency"),
-#                     "legal_form": ref.get("LegalForm"),
-#                     "activity_code": ref.get("ActivityCode"),
-#                     "model_type": ref.get("ModelType"),
-#                     "account_url": True if ref.get("AccountingDataURL") else False,
-#                     "legal_validation": ref.get("FullFillLegalValidation"),
-#                     "assembly_date": fnc.to_datetime(ref.get("GeneralAssemblyDate")),
-#                     "data_version": ref.get("DataVersion"),
-#                     "improvement_date": fnc.to_datetime(ref.get("ImprovementDate")),
-#                     "corrected_data": ref.get("CorrectedData"),
-#                     "last_update": datetime.now(),
-#                 })
-
-#             if len(ci_batch) >= batch_size:
-#                 fnc.write_batch(
-#                     engine=database_to.engine,
-#                     ci_batch=ci_batch,
-#                     statement_batch=statement_batch
-#                 )
-#                 ci_batch.clear()
-#                 statement_batch.clear()
-
-#         if ci_batch:
-#             fnc.write_batch(
-#                 engine=database_to.engine,
-#                 ci_batch=ci_batch,
-#                 statement_batch=statement_batch
-#             )
-
-
-# def statements_data_cd_nbb_data(db_from: str, db_to: str):
-#     """Populate all filings in CompanyData database."""
 
 #     database_from = DbConnector(db=db_from)
 #     database_to = DbConnector(db=db_to)
